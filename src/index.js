5317bdc3d61943bfb8dbedddeb07000acca42ff7
Louis Dijkshoorn <louis.dijkshoorn@gmail.com>
2026-06-16 16:00:22 +0200

    did I fix it now?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// Here is a nice comment, attempt 24
+// I am trying again, let's see again if this works
 function sayHi(name) {
-  return `Hello ${name}!`
+  return `Hello ${name}`
 }
 
 module.exports = sayHi
